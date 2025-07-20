d146824a9567f7dba576dc2356118697a43cd660
anand2312 <40204976+anand2312@users.noreply.github.com>
2025-07-20 18:55:49 +0530

    Make username, provider query parameters

--- aniwrap/api/watch_history.py
+++ aniwrap/api/watch_history.py
@@ -1,7 +1,7 @@
 from typing import Annotated, Literal
 
 from cattrs import unstructure
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Query
 
 from aniwrap.service.watch_history.anilist import AnilistWatchHistoryService
 
@@ -11,11 +11,11 @@ router = APIRouter(prefix="/watched")
 Provider = Literal["mal", "anilist"]
 
 
-@router.get("/{provider}/{username}")
+@router.get("/")
 async def get_watch_history(
-    provider: Annotated[Provider, Path(description="The anime tracking provider")],
+    provider: Annotated[Provider, Query(description="The anime tracking provider")],
     username: Annotated[
-        str, Path(description="The user's username on the specified platform")
+        str, Query(description="The user's username on the specified platform")
     ],
     watch_history_service: Annotated[AnilistWatchHistoryService, Depends()],
 ) -> dict:
--- aniwrap/api/wrapped.py
+++ aniwrap/api/wrapped.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Literal
 
-from fastapi import APIRouter, Depends, Path
+from fastapi import APIRouter, Depends, Query
 
 from aniwrap.service.stats import StatisticsService
 from aniwrap.service.watch_history.anilist import AnilistWatchHistoryService
@@ -12,11 +12,11 @@ router = APIRouter(prefix="/wrapped")
 Provider = Literal["mal", "anilist"]
 
 
-@router.get("/{provider}/{username}")
+@router.get("/")
 async def get_wrapped(
-    provider: Annotated[Provider, Path(description="The anime tracking provider")],
+    provider: Annotated[Provider, Query(description="The anime tracking provider")],
     username: Annotated[
-        str, Path(description="The user's username on the specified platform")
+        str, Query(description="The user's username on the specified platform")
     ],
     watch_history_service: Annotated[AnilistWatchHistoryService, Depends()],
     stats: Annotated[StatisticsService, Depends()],
